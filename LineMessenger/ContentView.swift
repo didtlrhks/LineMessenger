1887590556c01972c2f116d10a22f7bcd661d896
sikwan <yysskk99999@naver.com>
2024-03-10 21:49:13 +0900

    LineMessenger

---
+++ LineMessenger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LineMessenger
+//
+//  Created by 양시관 on 3/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LineMessenger/LineMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineMessengerApp.swift
+//  LineMessenger
+//
+//  Created by 양시관 on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LineMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
