7b8f4fc4023a4effc76e9ce11878456ff16e04b2
sikwan <yysskk99999@naver.com>
2024-03-12 00:41:11 +0900

    HomeVIew add

--- LineMessenger/View/Home/HomeView.swift
+++ LineMessenger/View/Home/HomeView.swift
@@ -16,6 +16,9 @@ struct HomeView : View {
         NavigationStack {
             ScrollView {
                 profileView
+                    .padding(.bottom,30)
+                
+                searchButton
             }.toolbar {
                 Image("bookmark")
                 Image("notifications")
@@ -28,13 +31,45 @@ struct HomeView : View {
             }
         }
     }
-    var profileView : some View {
-        HStack{
-            VStack(alignment: .leading,spacing: 7) {
-                Text("")
+    
+    var profileView: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 7) {
+                Text(viewModel.myUser?.name ?? "이름")
+                    .font(.system(size: 22, weight: .bold))
+                    .foregroundColor(.bkText)
+                Text(viewModel.myUser?.description ?? "상태 메시지 입력")
+                    .font(.system(size: 12))
+                    .foregroundColor(.greyDeep)
             }
+            Spacer()
+            
+            Image("person")
+                .frame(width:52,height: 52)
+                .clipShape(Circle())
         }
+        .padding(.horizontal,30)
     }
+    var searchButton : some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.clear)
+                .frame(height: 36)
+                .background(Color.greyCool)
+                .cornerRadius(5)
+            
+            HStack {
+                Text("검색")
+                    .font(.system(size: 12))
+                    .foregroundColor(.greyLightVer2)
+                Spacer()
+            }
+            .padding(.leading,22)
+        }
+        .padding(.horizontal,30)
+    }
+    
+    
 }
 
 
