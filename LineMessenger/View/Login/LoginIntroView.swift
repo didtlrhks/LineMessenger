70ff8453ffa7232e81139980dad8c8266abe81d2
sikwan <yysskk99999@naver.com>
2024-03-11 15:16:30 +0900

    LoginIntroView

--- LineMessenger/View/Login/LoginIntroView.swift
+++ LineMessenger/View/Login/LoginIntroView.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct LoginIntroView: View {
+    @State private var isPresentedLoginView : Bool = false
     var body: some View {
-        Text("Login")
+        NavigationStack{
+            VStack(spacing : 20) {
+                Spacer()
+                
+                Text("환영합니다.")
+                    .font(.system(size: 26,weight: .bold))
+                    .foregroundColor(.bkText)
+                Text("무료 메세지와 영상통화, 음성통화를 부담없이 즐겨보세요!")
+                    .font(.system(size:12))
+                    .foregroundColor(.greyDeep)
+                Spacer()
+                
+                Button{
+                    isPresentedLoginView.toggle()
+                } label: {
+                    Text("로그인")
+                        .font(.system(size: 14))
+                        .foregroundColor(.lineappcolor)
+                        .frame(maxWidth: .infinity, maxHeight: 40)
+                }
+                .overlay {
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.lineappcolor, lineWidth: 0.8)
+                }
+                .padding(.horizontal,15)
+            }
+            .navigationDestination(isPresented: $isPresentedLoginView){
+                LoginView()
+            }
+        }
     }
 }
 
---
+++ LineMessenger/View/Login/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  LineMessenger
+//
+//  Created by 양시관 on 3/11/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
